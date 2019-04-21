2b6d6047a4fe8a4ff31e840e002ab7379ff65f2d
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-21 17:26:33 +0900

    Hero 一覧で選択した Hero を詳細に表示するように変更

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,8 +9,12 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  hero: Hero = { id: 1, name: 'Windstorm' };
+  selectedHero: Hero;
   constructor() {}
 
   ngOnInit() {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
