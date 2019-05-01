80123d111fd982e43a312c36a21e143e66aebfae
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-01 14:38:23 +0900

    HeroService から MessageService にメッセージを登録するロジックにメソッドの抽出を適用

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,12 +12,16 @@ export class HeroService {
   constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
+    this.log('fetched heroes');
     return of(HEROES);
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(HEROES.find((hero) => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
