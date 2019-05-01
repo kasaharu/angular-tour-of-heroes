f2a12d2c1eabcbc40e6aabc12e82208b620c42f5
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-01 13:17:33 +0900

    SpyLocation を使った location back のテストを追加

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,16 +1,21 @@
+import { Location } from '@angular/common';
+import { SpyLocation } from '@angular/common/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { HeroDetailComponent } from './hero-detail.component';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let location = new SpyLocation();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule, RouterTestingModule],
       declarations: [HeroDetailComponent],
+      providers: [{ provide: Location, useValue: location }],
     }).compileComponents();
   }));
 
@@ -22,5 +27,13 @@ describe('HeroDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('call goBack() method', () => {
+    location.go('/path1');
+    location.go('/path2');
+    component.goBack();
+
+    expect(location.isCurrentPathEqualTo('/path1')).toBe(true);
   });
 });
