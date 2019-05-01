dc923f79384cfc664bc5d833dbed519cbba65170
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-01 17:09:38 +0900

    RouterTestingModule の依存解決

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeroSearchComponent } from './hero-search.component';
 
@@ -9,7 +10,7 @@ describe('HeroSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [HeroSearchComponent],
     }).compileComponents();
   }));
