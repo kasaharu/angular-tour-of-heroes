a413547de673b04f7018d339a5559a7cb252a9ef
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-01 17:59:23 +0900

    http.get() を使用している getHeroes() のテスト追加

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,10 +1,13 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
   let service: HeroService;
+  let httpTestingController: HttpTestingController;
+  const heroesUrl = 'api/heroes';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,9 +15,23 @@ describe('HeroService', () => {
     });
 
     service = TestBed.get(HeroService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('call getHeroes() method', () => {
+    const testHeroes: Hero[] = [{ id: 1, name: 'test' }];
+
+    service.getHeroes().subscribe((result) => expect(result).toEqual(testHeroes));
+    const req = httpTestingController.expectOne(heroesUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(testHeroes);
   });
 });
