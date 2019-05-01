4d7c318bc146cb0e3a8f2e162b69c370e179e9b6
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-01 17:13:20 +0900

    HeroService で対象 service を何度もインスタンス化しなくていいように beforeEach で実施

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -4,14 +4,17 @@ import { TestBed } from '@angular/core/testing';
 import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
+  let service: HeroService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
+
+    service = TestBed.get(HeroService);
   });
 
   it('should be created', () => {
-    const service: HeroService = TestBed.get(HeroService);
     expect(service).toBeTruthy();
   });
 });
