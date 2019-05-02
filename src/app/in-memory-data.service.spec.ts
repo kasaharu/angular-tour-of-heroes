f8266f31b3f0e992ebfd0e32ed30aa0208059654
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-02 15:21:45 +0900

    InMemoryDataService のテストを追加

--- src/app/in-memory-data.service.spec.ts
+++ src/app/in-memory-data.service.spec.ts
@@ -4,6 +4,18 @@ import { InMemoryDataService } from './in-memory-data.service';
 
 describe('InMemoryDataService', () => {
   let service: InMemoryDataService;
+  const heroes = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,5 +25,19 @@ describe('InMemoryDataService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('call createDb() method', () => {
+    expect(service.createDb()).toEqual({ heroes });
+  });
+
+  describe('call genId() method', () => {
+    it('引数が hero の配列の場合', () => {
+      expect(service.genId(heroes)).toBe(21);
+    });
+
+    it('引数が空配列の場合', () => {
+      expect(service.genId([])).toBe(11);
+    });
   });
 });
