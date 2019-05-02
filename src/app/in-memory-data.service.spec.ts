551db64dbe00e62d8fd66a689805cab498184e96
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-02 15:16:03 +0900

    InMemoryDataService で対象 service を何度もインスタンス化しなくていいように beforeEach で実施

--- src/app/in-memory-data.service.spec.ts
+++ src/app/in-memory-data.service.spec.ts
@@ -3,10 +3,15 @@ import { TestBed } from '@angular/core/testing';
 import { InMemoryDataService } from './in-memory-data.service';
 
 describe('InMemoryDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: InMemoryDataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+    service = TestBed.get(InMemoryDataService);
+  });
 
   it('should be created', () => {
-    const service: InMemoryDataService = TestBed.get(InMemoryDataService);
     expect(service).toBeTruthy();
   });
 });
