4d4e19f2178ffcc4fdd746108ff454f7361dc7cd
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-03 14:17:29 +0900

    NgRx Store から heroes を取得する query を実装

--- src/app/queries/hero.query.ts
+++ src/app/queries/hero.query.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+
+import { selectFeatureHeroes } from '../store/root/hero-store/state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroQuery {
-  constructor() {}
+  readonly heroes$ = this.store$.pipe(select(selectFeatureHeroes));
+
+  constructor(private store$: Store<{}>) {}
 }
