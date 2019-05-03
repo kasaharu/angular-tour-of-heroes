a457524ec2d267ea77ae888d9ab13905eaa5f63d
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-03 17:38:50 +0900

    hero 追加処理を usecase に移譲

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -20,14 +20,7 @@ export class HeroesComponent implements OnInit {
   }
 
   add(name: string): void {
-    name = name.trim();
-    if (!name) {
-      return;
-    }
-
-    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
-      this.heroes.push(hero);
-    });
+    this.heroesUsecase.addHero(name);
   }
 
   delete(hero: Hero): void {
--- src/app/usecases/heroes.usecase.ts
+++ src/app/usecases/heroes.usecase.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 
+import { Hero } from '../hero';
 import { HeroRepository } from '../repositories/hero.repository';
 import { HeroStoreActions } from '../store/root/hero-store';
 
@@ -11,6 +12,17 @@ export class HeroesUsecase {
   constructor(private store$: Store<{}>, private heroRepository: HeroRepository) {}
 
   async initialize() {
+    const result = await this.heroRepository.getHeroes().toPromise();
+    this.store$.dispatch(HeroStoreActions.fetchHeroes(result));
+  }
+
+  async addHero(name: string) {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+
+    await this.heroRepository.addHero({ name } as Hero).subscribe();
     const result = await this.heroRepository.getHeroes().toPromise();
     this.store$.dispatch(HeroStoreActions.fetchHeroes(result));
   }
