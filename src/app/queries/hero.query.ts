56a43da4ace83d82d3f7c133eb67cab81a4f0202
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-04 11:49:20 +0900

    NgRx Store から selectedHero を取得する query を実装

--- src/app/queries/hero.query.ts
+++ src/app/queries/hero.query.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
-import { selectFeatureHeroes } from '../store/root/hero-store/state';
+import { selectFeatureHeroes, selectFeatureSelectedHero } from '../store/root/hero-store/state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroQuery {
   readonly heroes$ = this.store$.pipe(select(selectFeatureHeroes));
+  readonly selectedHero$ = this.store$.pipe(select(selectFeatureSelectedHero));
 
   constructor(private store$: Store<{}>) {}
 }
