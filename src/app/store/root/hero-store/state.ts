689d29694fbb3d9d92e9f7eeb0c50fe72270bf3c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-04 11:48:00 +0900

    選択中の hero を保持する状態を追加

--- src/app/store/root/hero-store/state.ts
+++ src/app/store/root/hero-store/state.ts
@@ -3,14 +3,20 @@ import { Hero } from '../../../hero';
 
 export interface State {
   heroes: Hero[] | null;
+  selectedHero: Hero | null;
 }
 
 export const initialState: State = {
   heroes: null,
+  selectedHero: null,
 };
 
 export const selectFeature = createFeatureSelector<State>('hero');
 export const selectFeatureHeroes = createSelector(
   selectFeature,
   (state: State) => state.heroes,
+);
+export const selectFeatureSelectedHero = createSelector(
+  selectFeature,
+  (state: State) => state.selectedHero,
 );
